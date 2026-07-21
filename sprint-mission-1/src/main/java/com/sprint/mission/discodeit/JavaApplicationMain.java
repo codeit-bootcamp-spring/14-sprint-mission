2e9b93f5c22ec90d7648c737fa6317999930d8b9
김예준 <72857855+kim-yejunn@users.noreply.github.com>
2026-07-21 14:42:30 +0900

    fix: Controller 계층간에 이동 삭제 후 하나로 통합 - Controller -> Service -> Repository로 전달되게 변경

--- sprint-mission-1/src/main/java/com/sprint/mission/discodeit/JavaApplicationMain.java
+++ sprint-mission-1/src/main/java/com/sprint/mission/discodeit/JavaApplicationMain.java
@@ -5,9 +5,10 @@ import com.sprint.mission.discodeit.controller.ConsoleController;
 
 public class JavaApplicationMain {
     public static void main(String[] args) {
-        ConsoleController consoleContorller = new ConsoleController();
+        ConsoleController consoleController = new ConsoleController();
 
         System.out.println("--- 디스코드잇에 오신 걸 환영합니다 ---");
-        consoleContorller.consoleContorller();
+        consoleController.consoleContorller();
+        //수정
     }
 }
