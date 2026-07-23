aa0219078cf892602f49bdb78f1f992570f8fe0f
ParkSoomin <ssoomni52@gmail.com>
2026-07-23 09:51:51 +0900

    fix: 채널에 없는 유저가 메시지 보낼 수 있던 문제 수정

--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFMessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFMessageService.java
@@ -1,7 +1,55 @@
 package com.sprint.mission.discodeit.service.jcf;
 
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.entity.dto.message.MessageCreationDto;
+import com.sprint.mission.discodeit.entity.dto.message.MessageUpdateDto;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.repository.MessageRepository;
 import com.sprint.mission.discodeit.service.MessageService;
+import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RequiredArgsConstructor
 public class JCFMessageService implements MessageService {
+    private final MessageRepository messageRepository;
+    private final ChannelRepository channelRepository;
 
+    @Override
+    public Message createMessage(MessageCreationDto dto) {
+        String content = dto.getContent();
+        UUID userId = dto.getUserId();
+
+        Message message = new Message(content, userId);
+        UUID messageId = message.getId();
+
+        Channel channel = channelRepository.findById(dto.getChannelId())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 채널입니다."));
+
+        channel.addMessage(userId, messageId);
+        return messageRepository.create(message);
+    }
+
+    @Override
+    public Optional<Message> getMessage(UUID id) {
+        return messageRepository.findById(id);
+    }
+
+    @Override
+    public List<Message> getAllMessages() {
+        return messageRepository.findAll();
+    }
+
+    @Override
+    public void updateMessage(UUID id, MessageUpdateDto dto) {
+        messageRepository.update(id, dto);
+    }
+
+    @Override
+    public void deleteMessage(UUID id) {
+        messageRepository.deleteById(id);
+    }
 }
