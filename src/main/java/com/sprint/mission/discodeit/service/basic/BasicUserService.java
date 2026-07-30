305a7995c3a85fe71ed120d93a93d89fc8a62c50
ParkSoomin <ssoomni52@gmail.com>
2026-07-30 14:16:30 +0900

    docs(UserService): UserService 고도화 추가 요구사항 TODO 작성

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
@@ -23,30 +23,38 @@ public class BasicUserService implements UserService {
 
     @Override
     public User createAccount(UserCreationDto dto) {
+        // TODO 1. 선택적으로 프로필 이미지를 등록할 수 있어야 한다.
+        // TODO 2. DTO를 활용해 파라미터 그룹화해야 한다.
+        // TODO 3. username과 email은 다른 유저와 달라야 한다.
+        // TODO 4. UserStatus를 같이 생성해야 한다.
         User user = new User(dto.getName());
         return userRepository.create(user);
     }
 
     @Override
     public Optional<User> getUser(UUID id) {
+        // TODO 1. 사용자 온라인 정보를 포함시켜야 한다.
+        // TODO 2. 패스워드 정보는 제외해야 한다.
         return userRepository.findById(id);
     }
 
     @Override
     public List<User> getAllUsers() {
+        // TODO 1. 사용자 온라인 정보를 포함시켜야 한다.
+        // TODO 2. 패스워드 정보는 제외해야 한다.
         return userRepository.findAll();
     }
 
     @Override
     public void updateUser(UUID id, UserUpdateDto dto) {
+        // TODO 1. 선택적으로 프로필 이미지를 대체할 수 있어야 한다.
+        // TODO 2. DTO를 활용해 파라미터를 그룹화한다.
         userRepository.updateName(id, dto.getName());
     }
 
     @Override
     public void deleteAccount(UUID id) {
-        // 유저 삭제 시,
-        // 1. 해당 유저의 message를 삭제해야 한다.
-        // 2. 모든 channel에서 해당 유저를 삭제해야 한다.
+        // TODO 1. 관련된 도메인도 같이 삭제한다. (BinaryContent, UserStatus, ReadStatus ... )
         channelRepository.deleteUsersByUserId(id);
         messageRepository.deleteAllByUserId(id);
         userRepository.deleteById(id);
