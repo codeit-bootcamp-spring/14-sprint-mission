c7f81945904833cbe19c76a14c699af612d37eee
ParkSoomin <ssoomni52@gmail.com>
2026-08-04 16:38:04 +0900

    refactor(FileReadStatusRepository): 빈으로 등록

--- src/main/java/com/sprint/mission/discodeit/repository/file/FileReadStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileReadStatusRepository.java
@@ -2,8 +2,9 @@ package com.sprint.mission.discodeit.repository.file;
 
 import com.sprint.mission.discodeit.entity.ReadStatus;
 import com.sprint.mission.discodeit.repository.ReadStatusRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public class FileReadStatusRepository extends AbstractFileRepository<ReadStatus>
         implements ReadStatusRepository {
     protected FileReadStatusRepository() {
