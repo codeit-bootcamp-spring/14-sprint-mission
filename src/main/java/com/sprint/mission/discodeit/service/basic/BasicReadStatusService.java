60d27a7231ac52beb988601637be9d3771601736
ParkSoomin <ssoomni52@gmail.com>
2026-08-05 11:23:37 +0900

    refactor(ReadStatusService): 인터페이스 정의

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicReadStatusService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicReadStatusService.java
@@ -7,6 +7,7 @@ import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.repository.ChannelRepository;
 import com.sprint.mission.discodeit.repository.ReadStatusRepository;
 import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.service.ReadStatusService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,11 +17,12 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-public class BasicReadStatusService {
+public class BasicReadStatusService implements ReadStatusService {
     private final ReadStatusRepository readStatusRepository;
     private final UserRepository userRepository;
     private final ChannelRepository channelRepository;
 
+    @Override
     public ReadStatus create(@Valid ReadStatusCreateDto dto) {
         User user = userRepository.findById(dto.getUserId())
                 .orElseThrow();
@@ -34,15 +36,18 @@ public class BasicReadStatusService {
         return dto.toReadStatus();
     }
 
+    @Override
     public ReadStatus getReadStatus(UUID id) {
         return readStatusRepository.findById(id)
                 .orElseThrow();
     }
 
+    @Override
     public List<ReadStatus> getAllReadStatusByUserId(UUID userId) {
         return readStatusRepository.findAllByUserId(userId);
     }
 
+    @Override
     public void updateReadStatus(UUID id) {
         // 요구사항 : DTO를 활용해 파라미터를 그룹화합니다 (수정 대상 객체의 id 파라미터, 수정할 값 파라미터)
         // 아니 근데 readStatus애서 수정할 필드라곤 마지막 메시지 읽은 시간밖에 없어서 파라미터도 필요 없는데
@@ -50,6 +55,7 @@ public class BasicReadStatusService {
         readStatusRepository.update(id);
     }
 
+    @Override
     public void deleteReadStatus(UUID id) {
         readStatusRepository.deleteById(id);
     }
