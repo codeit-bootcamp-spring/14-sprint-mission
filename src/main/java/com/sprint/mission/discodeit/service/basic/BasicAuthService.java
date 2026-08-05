8a090edd1cbaf47fd21bc6ddc8fd5dda800b218e
ParkSoomin <ssoomni52@gmail.com>
2026-08-05 15:13:38 +0900

    refactor(AuthService): 커스텀 예외 전략 적용

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
@@ -4,14 +4,14 @@ import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.entity.UserStatus;
 import com.sprint.mission.discodeit.dto.user.UserLoginRequestDto;
 import com.sprint.mission.discodeit.dto.user.UserResponseDto;
+import com.sprint.mission.discodeit.exception.CustomException;
+import com.sprint.mission.discodeit.exception.ExceptionType;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.repository.UserStatusRepository;
 import com.sprint.mission.discodeit.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
@@ -22,9 +22,9 @@ public class BasicAuthService implements AuthService {
     @Override
     public UserResponseDto login(@Valid UserLoginRequestDto dto) {
         User retrieved = userRepository.findByNameAndPassword(dto.getName(), dto.getPassword())
-                .orElseThrow(() -> new NoSuchElementException(String.format("잘못된 name 혹은 password")));
+                .orElseThrow(() -> new CustomException(ExceptionType.USER_NOT_FOUND_IN_DATABASE));
         UserStatus userStatus = userStatusRepository.findByUserId(retrieved.getId())
-                .orElseThrow(() -> new NoSuchElementException(String.format("잘못된 userId")));
+                .orElseThrow(() -> new CustomException(ExceptionType.USERSTATUS_NOT_FOUND_IN_DATABASE));
         userStatus.updateLastSeenAt();
         return UserResponseDto.of(retrieved, userStatus);
     }
