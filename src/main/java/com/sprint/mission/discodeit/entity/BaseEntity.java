d81d7be7612004c4c4f5572851315a4404f9b304
kjh940222 <kjh940222@gmail.com>
2026-08-06 01:11:28 +0900

    Codeit sprint mission 2-2 (refactor) : Entity의 공통 필드와 메서드를 BaseEntity 추상 클래스를 생성하여 코드 수정

---
+++ src/main/java/com/sprint/mission/discodeit/entity/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.sprint.mission.discodeit.entity;
+
+import lombok.Getter;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Getter
+public abstract class BaseEntity implements Serializable {
+    // 직렬화
+    // 외부에서 접근할 수 없고, 클래스 하나만 가지고, 상수이어야 하는 직렬화
+    private static final long serialVersionID = 1L;
+
+    // 각 Entity의 공통 필드
+    private final UUID id;
+    private final Long createdAt;
+    private Long updatedAt;
+
+    protected BaseEntity() {
+        this.id = UUID.randomUUID();
+        this.createdAt = System.currentTimeMillis();
+        this.updatedAt = createdAt;
+    }
+
+    protected void updateTime() {
+        this.updatedAt = System.currentTimeMillis();
+    }
+}
--- src/main/java/com/sprint/mission/discodeit/entity/Message.java
+++ src/main/java/com/sprint/mission/discodeit/entity/Message.java
@@ -1,44 +1,32 @@
 package com.sprint.mission.discodeit.entity;
 
 import lombok.Getter;
-import lombok.Setter;
 
-import java.io.Serializable;
 import java.util.UUID;
 
 @Getter
-public class Message implements Serializable {
-    // 직렬화
-    private static final long serialVersionID = 1L;
-
-    private UUID id;
-
-    // 메시지 내용, 사용자 ID, 채널 ID
+public class Message extends BaseEntity {
+    // 메시지 내용, 채널 ID, 글쓴이 ID
     private String content;
-    private UUID userId;
     private UUID channelId;
-
-    private Long createdAt;
-    private Long updatedAt;
+    private UUID authorId;
 
     // 생성자
-    public Message(String content, UUID userId, UUID channelId) {
-        this.id = UUID.randomUUID();
+    public Message(String content, UUID channelId, UUID authorId) {
+        super();
         this.content = content;
-        this.userId = userId;
         this.channelId = channelId;
-        this.createdAt = System.currentTimeMillis();
-        this.updatedAt = createdAt;
+        this.authorId = authorId;
     }
 
     // update()
     public void updateContent(String content) {
         this.content = content;
-        this.updatedAt = System.currentTimeMillis();
+        super.updateTime();
     }
 
     @Override
     public String toString() {
-        return "Message{id=" + this.id + ", content=" + this.content + ", userId=" + this.userId + ", channelId=" + this.channelId + ", createdAt=" + this.createdAt + ", updatedAt=" + this.updatedAt + "}";
+        return "Message{id=" + super.getId() + ", content=" + this.content + ", channelId=" + this.channelId + ", authorId=" + this.authorId + ", createdAt=" + super.getCreatedAt() + ", updatedAt=" + super.getUpdatedAt() + "}";
     }
 }
