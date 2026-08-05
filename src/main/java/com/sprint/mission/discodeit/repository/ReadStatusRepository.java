3d44a2d8f0123bb2b539964113ad70c1b76eb28c
ParkSoomin <ssoomni52@gmail.com>
2026-08-05 10:39:22 +0900

    feat(ReadStatusService): create 요구사힝 1. DTO를 활용해 파라미터를 그룹화합니다. 2. 관련된 Channel이나 User가 존재하지 않으면 예외를 발생시킵니다. 3. 같은 Channel과 User와 관련된 객체가 이미 존재하면 예외를 발생시킵니다.

--- src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.UUID;
 
 public interface ReadStatusRepository extends CrudRepository<ReadStatus> {
+    boolean existsByUserAndChannel(UUID userId, UUID channelId);
 
     List<ReadStatus> createAll(List<ReadStatus> readStatuses);
 
---
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicReadStatusService.java
@@ -0,0 +1,35 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.dto.readStatus.ReadStatusCreateDto;
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.ReadStatus;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+import com.sprint.mission.discodeit.repository.ReadStatusRepository;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BasicReadStatusService {
+    private final ReadStatusRepository readStatusRepository;
+    private final UserRepository userRepository;
+    private final ChannelRepository channelRepository;
+
+    public ReadStatus create(@Valid ReadStatusCreateDto dto) {
+        User user = userRepository.findById(dto.getUserId())
+                .orElseThrow();
+        Channel channel = channelRepository.findById(dto.getChannelId())
+                .orElseThrow();
+
+        if (readStatusRepository.existsByUserAndChannel(user.getId(), channel.getId())) {
+            throw new IllegalArgumentException("ReadStatus 이미 있어서 못만듦 ㅋ");
+        }
+
+        return dto.toReadStatus();
+    }
+
+
+}
