64991bfd8bb19d1166a655be7394792029c048d9
joyejun <joyejun000@gmail.com>
2026-08-12 15:15:10 +0900

    UserStatus 도메인 추가

--- codeit/src/main/java/com/sprint/mission/discodeit/entity/UserStatus.java
+++ codeit/src/main/java/com/sprint/mission/discodeit/entity/UserStatus.java
@@ -1,13 +1,33 @@
 package com.sprint.mission.discodeit.entity;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.util.UUID;
 
 public class UserStatus extends BaseEntity {
     private UUID userId;
+    private Instant lastLoginCheckTime;
+    private boolean loginCheck;
 
     protected UserStatus(UUID id, Integer userNum, UUID userId) {
         super(id, userNum);
         this.userId = userId;
     }
+
+    public Instant loginCkeck() {
+        this.lastLoginCheckTime = Instant.now();
+        return lastLoginCheckTime;
+    }
+
+    public boolean lastLoginCheck() {
+        Instant nowCheckTime = Instant.now();
+        Duration duration = Duration.between(this.lastLoginCheckTime, nowCheckTime);
+        long diffSeconds = duration.getSeconds();
+        if (300 >= diffSeconds) {
+            return loginCheck = true;
+        } else {
+            return loginCheck = false;
+        }
+    }
+
 }
