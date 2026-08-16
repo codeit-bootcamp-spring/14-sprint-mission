724368687894f4e386dbcac1f803d83175ef36ec
rladjwls02 <safhao7152@gmail.com>
2026-08-16 18:34:50 +0900

    feat: ReadStatusController 구현 - 메세지 수신정보 API 추가

---
+++ src/main/java/com/sprint/mission/discodeit/controller/ReadStatusController.java
@@ -0,0 +1,34 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.sprint.mission.discodeit.dto.readstatusdto.ReadStatusCreateRequestDto;
+import com.sprint.mission.discodeit.dto.readstatusdto.ReadStatusResponseDto;
+import com.sprint.mission.discodeit.dto.readstatusdto.ReadStatusUpdateRequestDto;
+import com.sprint.mission.discodeit.service.ReadStatusService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/readStatus")
+public class ReadStatusController {
+    private final ReadStatusService readStatusService;
+
+    @RequestMapping(method = RequestMethod.POST, value = "")
+    public ReadStatusResponseDto create(@RequestBody ReadStatusCreateRequestDto dto) {
+        return readStatusService.createReadStatus(dto);
+    }
+
+    @RequestMapping(method = RequestMethod.PATCH, value = "")
+    public ReadStatusResponseDto modify(@PathVariable UUID id,
+                                        @RequestBody ReadStatusUpdateRequestDto dto) {
+        return readStatusService.updateReadStatus(id,dto);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "")
+    public List<ReadStatusResponseDto> read(@RequestParam UUID userId) {
+        return readStatusService.findAllByUserId(userId);
+    }
+}
