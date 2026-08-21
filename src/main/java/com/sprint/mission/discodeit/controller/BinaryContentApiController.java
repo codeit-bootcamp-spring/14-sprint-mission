22d50cc57ebbe71d7f2ea5c83b4b281b0602f855
ParkSoomin <ssoomni52@gmail.com>
2026-08-21 19:16:19 +0900

    refactor(BinaryContentApiContoller) 요구사항 맞춰 api 수정

--- src/main/java/com/sprint/mission/discodeit/controller/BinaryContentApiController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentApiController.java
@@ -1,18 +1,12 @@
 package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.domain.binaryContent.BinaryContent;
-import com.sprint.mission.discodeit.dto.binaryContent.BinaryContentRequestDto;
 import com.sprint.mission.discodeit.dto.binaryContent.BinaryContentResponseDto;
 import com.sprint.mission.discodeit.service.basic.BasicBinaryContentService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -23,9 +17,10 @@ import java.util.UUID;
 public class BinaryContentApiController {
     private final BasicBinaryContentService binaryContentService;
 
+    @ResponseStatus(HttpStatus.OK)
     @RequestMapping(method = RequestMethod.GET)
-    public List<BinaryContentResponseDto> getBinaryContents(@Valid @RequestBody BinaryContentRequestDto request) {
-        return binaryContentService.getAllBinaryContents(request.binaryContentIds());
+    public List<BinaryContentResponseDto> getBinaryContents(@RequestParam List<UUID> binaryContentIds) {
+        return binaryContentService.getAllBinaryContents(binaryContentIds);
     }
 
     // 심화 요구사항
