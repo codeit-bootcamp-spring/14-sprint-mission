64091a1ca6b7d2eea8eb012c67006d6419e08227
ParkSoomin <ssoomni52@gmail.com>
2026-08-21 20:44:57 +0900

    feat(MessageResponseDto) API 스펙에 맞춰 응답 필드 추가

--- src/main/java/com/sprint/mission/discodeit/dto/message/MessageResponseDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/message/MessageResponseDto.java
@@ -4,19 +4,26 @@ import com.sprint.mission.discodeit.domain.message.Message;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
 public record MessageResponseDto(@NotNull UUID id,
+                                 @NotNull Instant createdAt,
+                                 @NotNull Instant updatedAt,
                                  @NotBlank String content,
                                  @NotNull UUID channelId,
-                                 @NotNull UUID authorId) {
+                                 @NotNull UUID authorId,
+                                 @NotNull List<UUID> attachmentIds) {
     public static MessageResponseDto of(Message message) {
         return new MessageResponseDto(
                 message.getId(),
+                message.getCreatedAt(),
+                message.getUpdatedAt(),
                 message.getContent(),
                 message.getChannelId(),
-                message.getUserId()
+                message.getUserId(),
+                message.getAttachmentIds()
         );
     }
 }
