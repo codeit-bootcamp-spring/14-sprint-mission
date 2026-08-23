e0b6a5accc830673669b6a4d084b698325fa9082
rladjwls02 <safhao7152@gmail.com>
2026-08-23 18:53:01 +0900

    refactor: OpenAPI 스펙에 맞춰 Request DTO 필드명 리팩토링

--- src/main/java/com/sprint/mission/discodeit/dto/userdto/UserCreateRequestDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/userdto/UserCreateRequestDto.java
@@ -10,16 +10,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserCreateRequestDto {
+    // OpenAPI 규격 맞추기 위해 필요한 필드 추가
     private String email;
-    private String name;
-
+    private String username;
+    private String password;
     private BinaryContentCreateRequestDto binaryContentCreateRequestDto;
 
-    public UserCreateRequestDto(String email, String name) {
-        this(email, name, null);
+    // 이미지 파일 없이 유저 생성자
+    public UserCreateRequestDto(String email, String username, String password) {
+        this(email, username, password, null);
     }
 
+    //TODO:나중에 password도 넣어야됌.. 일단은 dto만 맞추기
     public User toEntity() {
-        return new User(this.email, this.name);
+        return new User(this.email, this.username);
     }
 }
