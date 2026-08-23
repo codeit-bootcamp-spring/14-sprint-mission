47e0e3cd6c33775e3a9676f8052681bc16167032
ParkSoomin <ssoomni52@gmail.com>
2026-08-23 16:48:19 +0900

    fix(ReadStatusResponse): response가 api 계약과 일치하도록 수정

--- src/main/java/com/sprint/mission/discodeit/dto/readStatus/ReadStatusResponse.java
+++ src/main/java/com/sprint/mission/discodeit/dto/readStatus/ReadStatusResponse.java
@@ -1,20 +1,25 @@
 package com.sprint.mission.discodeit.dto.readStatus;
 
 import com.sprint.mission.discodeit.domain.readstatus.ReadStatus;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.Instant;
 import java.util.UUID;
 
-public record ReadStatusResponse(UUID id,
-                                 UUID userId,
-                                 UUID channelId,
+public record ReadStatusResponse(@NotNull UUID id,
+                                 @NotNull Instant createdAt,
+                                 @NotNull Instant updatedAt,
+                                 @NotNull UUID userId,
+                                 @NotNull UUID channelId,
                                  Instant lastReadAt) {
-    public static ReadStatusResponse of(ReadStatus readStatus) {
+    public static ReadStatusResponse of(ReadStatus created) {
         return new ReadStatusResponse(
-                readStatus.getId(),
-                readStatus.getUserId(),
-                readStatus.getChannelId(),
-                readStatus.getLastReadAt()
+                created.getId(),
+                created.getCreatedAt(),
+                created.getUpdatedAt(),
+                created.getUserId(),
+                created.getChannelId(),
+                created.getLastReadAt()
         );
     }
 }
