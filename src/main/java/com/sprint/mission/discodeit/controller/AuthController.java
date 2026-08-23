4fbe588d191a911b111b2717575862abd75ed3e2
rladjwls02 <safhao7152@gmail.com>
2026-08-23 19:09:25 +0900

    refactor: OpenAPI 스펙에 맞춰 Controller 매핑 및 HTTP Status 수정

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -5,7 +5,6 @@ import com.sprint.mission.discodeit.dto.userdto.UserResponseDto;
 import com.sprint.mission.discodeit.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -15,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final AuthService authService;
 
-    @RequestMapping(method = RequestMethod.POST, value = "/auth")
+    @RequestMapping(method = RequestMethod.POST, value = "/login" )
     public UserResponseDto login(@RequestBody AuthCreateRequestDto dto) {
         return authService.login(dto);
     }
--- src/main/java/com/sprint/mission/discodeit/controller/ChannelController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/ChannelController.java
@@ -1,12 +1,12 @@
 package com.sprint.mission.discodeit.controller;
 
-
 import com.sprint.mission.discodeit.dto.channeldto.ChannelCreateRequestDto;
 import com.sprint.mission.discodeit.dto.channeldto.ChannelResponseDto;
 import com.sprint.mission.discodeit.dto.channeldto.ChannelUpdateRequestDto;
 import com.sprint.mission.discodeit.dto.channeldto.PrivateChannelCreateRequestDto;
 import com.sprint.mission.discodeit.service.ChannelService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,28 +19,31 @@ public class ChannelController {
     private final ChannelService channelService;
 
     @RequestMapping(method = RequestMethod.POST, value = "/public")
+    @ResponseStatus(HttpStatus.CREATED)
     public ChannelResponseDto createPublic(@RequestBody ChannelCreateRequestDto dto) {
         return channelService.createPublicChannel(dto);
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/private")
+    @ResponseStatus(HttpStatus.CREATED)
     public ChannelResponseDto createPrivate(@RequestBody PrivateChannelCreateRequestDto dto) {
         return channelService.createPrivateChannel(dto);
     }
 
-    @RequestMapping(method = RequestMethod.PATCH, value = "/{id}")
-    public ChannelResponseDto update(@PathVariable UUID id,
+    @RequestMapping(method = RequestMethod.PATCH, value = "/{channelId}")
+    public ChannelResponseDto update(@PathVariable UUID channelId,
                                      @RequestBody ChannelUpdateRequestDto dto) {
-        return channelService.updateChannel(id, dto);
+        return channelService.updateChannel(channelId, dto);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
-    public void delete(@PathVariable UUID id) {
-        channelService.deleteChannel(id);
+    @RequestMapping(method = RequestMethod.DELETE, value = "/{channelId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable UUID channelId) {
+        channelService.deleteChannel(channelId);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "")
-    public List<ChannelResponseDto> getChannelListByUserId(@RequestParam UUID id) {
-        return channelService.findAllByUserId(id);
+    public List<ChannelResponseDto> getChannelListByUserId(@RequestParam UUID userId) {
+        return channelService.findAllByUserId(userId);
     }
 }
